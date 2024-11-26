617d05ce17df8750635bc9b6ac7c16e6e7b341dd
Mouaz Aldakkak <apothekendatabase@gmail.com>
2024-11-26 20:31:34 +0100

    feat: add create user route with its schema

--- src/routes/users/users.index.ts
+++ src/routes/users/users.index.ts
@@ -3,6 +3,8 @@ import { createRouter } from "@/lib/createApp";
 import * as handlers from "./users.handler";
 import * as routes from "./users.routes";
 
-const router = createRouter().openapi(routes.list, handlers.list);
+const router = createRouter()
+  .openapi(routes.list, handlers.list)
+  .openapi(routes.create, handlers.create);
 
 export default router;
--- src/routes/users/users.routes.ts
+++ src/routes/users/users.routes.ts
@@ -1,10 +1,21 @@
-import { createRoute } from "@hono/zod-openapi";
+import { createRoute, z } from "@hono/zod-openapi";
 
 import { httpStatusCode } from "@/utils/constants";
 
 import { usersSchema } from "./users.handler";
 
 const tags = ["Users"];
+
+const createSchema = z.object({
+  message: z.string(),
+});
+
+const newUserSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  subscribed: z.boolean(),
+  mevAccepted: z.boolean(),
+});
 
 export const list = createRoute({
   path: "/api/users",
@@ -22,4 +33,35 @@ export const list = createRoute({
   },
 });
 
+export const create = createRoute({
+  path: "/api/users",
+  tags,
+  method: "post",
+  request: {
+    body: {
+      application: "json",
+      required: true,
+      content: {
+        "application/json": {
+          schema: newUserSchema,
+        },
+      },
+      description: "New User",
+    },
+  },
+  responses: {
+    [httpStatusCode.CREATED]: {
+      content: {
+        "application/json": {
+          schema: createSchema,
+        },
+      },
+      description: "Successfully Created",
+    },
+
+  },
+});
+
 export type UserRoute = typeof list;
+
+export type UserRouteCreate = typeof create;

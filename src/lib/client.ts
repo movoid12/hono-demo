dc2462a2620e49e9cda635a9240b6ea055f7614a
Mouaz Aldakkak <apothekendatabase@gmail.com>
2024-11-26 16:22:32 +0100

    feat: add rpc example to get fully type-safe routes

---
+++ src/lib/client.ts
@@ -0,0 +1,13 @@
+// hono client rpc example
+// needs to be tested on a monorepo to share types between server and client
+
+import { hc } from "hono/client";
+
+import type { AppType } from "@/app";
+
+const client = hc<AppType>("http://localhost:3000/");
+
+//* this is useful to be used in a monorepo
+//* and has possibilty to generate an fully type-safe sdk and call an api from the client side
+
+export default client;

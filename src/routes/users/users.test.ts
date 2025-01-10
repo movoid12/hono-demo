d7cfc998c0efa3d1e369ea22de7f203d9274458a
Mouaz Aldakkak <apothekendatabase@gmail.com>
2025-01-10 16:27:12 +0100

    refactor: improve test file and order

--- src/routes/users/users.test.ts
+++ src/routes/users/users.test.ts
@@ -1,6 +1,6 @@
 /* eslint-disable ts/ban-ts-comment */
 import { testClient } from 'hono/testing';
-import { expect, expectTypeOf, it } from 'vitest';
+import { expect, expectTypeOf, it, describe } from 'vitest';
 
 import { createRouter } from '../../openapi/helpers/create-router';
 import {
@@ -12,91 +12,97 @@ import router from './users.index';
 
 const client = testClient(createRouter().route('/', router));
 
-it('should return a list of users', async () => {
-  const response = await client.api.users.$get();
 
-  expect(response.status).toBe(httpStatusCode.OK);
+describe('Route -> api/users/', () => {
+  it('GET: api/users/ should return a list of users', async () => {
+    const response = await client.api.users.$get();
 
-  if (response.status === httpStatusCode.OK) {
-    const json = await response.json();
+    expect(response.status).toBe(httpStatusCode.OK);
 
-    expectTypeOf(json).toBeArray();
+    if (response.status === httpStatusCode.OK) {
+      const json = await response.json();
 
-    expect(json.length).toBe(4);
-  }
-});
+      expectTypeOf(json).toBeArray();
 
-//* get one user
-
-it('get api/users/{id} returns a user', async () => {
-  const response = await client.api.users[':id'].$get({
-    param: {
-      id: 2,
-    },
+      expect(json.length).toBe(4);
+    }
   });
 
-  expect(response.status).toBe(httpStatusCode.OK);
 
-  if (response.status === httpStatusCode.OK) {
-    const json = await response.json();
+  //* get one user
 
-    expect(json.name).toBe('Jane Top');
-  }
-});
+  it('GET: api/users/:id returns a user', async () => {
+    const response = await client.api.users[':id'].$get({
+      param: {
+        id: 2,
+      },
+    });
 
-it('get api/users/{id} validates the id param', async () => {
-  const response = await client.api.users[':id'].$get({
-    param: {
+    expect(response.status).toBe(httpStatusCode.OK);
+
+    if (response.status === httpStatusCode.OK) {
+      const json = await response.json();
+
+      expect(json.name).toBe('Jane Top');
+    }
+  });
+
+  it('GET: api/users/:id validates the id param', async () => {
+    const response = await client.api.users[':id'].$get({
+      param: {
+        // @ts-expect-error
+        id: 'abc',
+      },
+    });
+
+    expect(response.status).toBe(httpStatusCode.UNPROCESSABLE_ENTITY);
+
+    if (response.status === httpStatusCode.UNPROCESSABLE_ENTITY) {
+      const json = await response.json();
       // @ts-expect-error
-      id: 'abc',
-    },
+      expect(json.error.issues[0].path[0]).toBe('id');
+      // @ts-expect-error
+      expect(json.error.issues[0].message).toBe(
+        ZOD_ERROR_MESSAGES.EXPECTED_NUMBER,
+      );
+    }
   });
 
-  expect(response.status).toBe(httpStatusCode.UNPROCESSABLE_ENTITY);
+  it('GET: api/users/:id returns a not found message', async () => {
+    const response = await client.api.users[':id'].$get({
+      param: {
+        id: 5,
+      },
+    });
 
-  if (response.status === httpStatusCode.UNPROCESSABLE_ENTITY) {
-    const json = await response.json();
-    // @ts-expect-error
-    expect(json.error.issues[0].path[0]).toBe('id');
-    // @ts-expect-error
-    expect(json.error.issues[0].message).toBe(
-      ZOD_ERROR_MESSAGES.EXPECTED_NUMBER,
-    );
-  }
-});
+    expect(response.status).toBe(httpStatusCode.NOT_FOUND);
 
-it('get api/users/{id} returns a not found message', async () => {
-  const response = await client.api.users[':id'].$get({
-    param: {
-      id: 5,
-    },
+    if (response.status === httpStatusCode.NOT_FOUND) {
+      const json = await response.json();
+
+      expect(json.message).toBe(httpStatusMessages.NOT_FOUND);
+    }
   });
 
-  expect(response.status).toBe(httpStatusCode.NOT_FOUND);
+  //* create a new user
 
-  if (response.status === httpStatusCode.NOT_FOUND) {
-    const json = await response.json();
+  it('POST: api/users/ create a new user and api/users route should returns a success message', async () => {
+    const response = await client.api.users.$post({
+      json: {
+        id: 5,
+        name: 'Jane Top',
+        email: 'tGZQO@example.com',
+        subscribed: true,
+        mevAccepted: true,
+      },
+    });
 
-    expect(json.message).toBe(httpStatusMessages.NOT_FOUND);
-  }
+    if (response.status === httpStatusCode.CREATED) {
+      const json = await response.json();
+
+      expect(json.message).toBe(httpStatusMessages.CREATED);
+    }
+  })
+
 });
 
-//* create a new user
-
-it('create a new user and api/users route should returns a success message', async () => {
-  const response = await client.api.users.$post({
-    json: {
-      id: 5,
-      name: 'Jane Top',
-      email: 'tGZQO@example.com',
-      subscribed: true,
-      mevAccepted: true,
-    },
-  });
-
-  if (response.status === httpStatusCode.CREATED) {
-    const json = await response.json();
-
-    expect(json.message).toBe(httpStatusMessages.CREATED);
-  }
-});

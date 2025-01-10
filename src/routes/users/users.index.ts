563ff856c9bfe3d212e9578d77ee89caf87e3ac3
Mouaz Aldakkak <apothekendatabase@gmail.com>
2025-01-10 15:38:39 +0100

    refactor: fix sorting

--- src/routes/users/users.index.ts
+++ src/routes/users/users.index.ts
@@ -5,7 +5,47 @@ import {
 import { httpStatusCode, httpStatusMessages } from '../../utils/constants';
 import * as routes from './users.routes';
 
-const users = [
+//* Handlers
+const getUsersList: AppRouteHandler<typeof routes.list> = (c) => {
+  // TODO: add getting list function when integrating with a database
+  return c.json(usersMock);
+};
+
+const createUser: AppRouteHandler<typeof routes.create> = (c) => {
+  // TODO: add create/insert function when integrating with a database
+
+  return c.json(
+    {
+      message: httpStatusMessages.CREATED,
+    },
+    httpStatusCode.CREATED,
+  );
+};
+
+const getUser: AppRouteHandler<typeof routes.getOne> = (c) => {
+  const { id } = c.req.valid('param');
+
+  const user = usersMock.find((user) => user.id === id);
+
+  if (!user) {
+    return c.json(
+      {
+        message: httpStatusMessages.NOT_FOUND,
+      },
+      httpStatusCode.NOT_FOUND,
+    );
+  }
+
+  return c.json(user, httpStatusCode.OK);
+};
+
+// * Routes
+const users = createRouter()
+  .openapi(routes.list, getUsersList)
+  .openapi(routes.create, createUser)
+  .openapi(routes.getOne, getUser);
+
+const usersMock = [
   {
     id: 1,
     name: 'John Doe',
@@ -36,42 +76,4 @@ const users = [
   },
 ];
 
-const getUsersList: AppRouteHandler<typeof routes.list> = (c) => {
-  // TODO: add getting list function when integrating with a database
-  return c.json(users);
-};
-
-const createUser: AppRouteHandler<typeof routes.create> = (c) => {
-  // TODO: add create/insert function when integrating with a database
-
-  return c.json(
-    {
-      message: httpStatusMessages.CREATED,
-    },
-    httpStatusCode.CREATED,
-  );
-};
-
-const getUser: AppRouteHandler<typeof routes.getOne> = (c) => {
-  const { id } = c.req.valid('param');
-
-  const user = users.find((user) => user.id === id);
-
-  if (!user) {
-    return c.json(
-      {
-        message: httpStatusMessages.NOT_FOUND,
-      },
-      httpStatusCode.NOT_FOUND,
-    );
-  }
-
-  return c.json(user, httpStatusCode.OK);
-};
-
-const router = createRouter()
-  .openapi(routes.list, getUsersList)
-  .openapi(routes.create, createUser)
-  .openapi(routes.getOne, getUser);
-
-export default router;
+export default users;
